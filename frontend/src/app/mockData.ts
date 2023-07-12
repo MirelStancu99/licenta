d4dab29dd8d4e681a04ceb2c780a0beeddae4a63
Mirel Stancu <stancumirel26@gmai.com>
2023-07-12 20:55:37 +0300

    refactoring

--- frontend/src/app/mockData.ts
+++ frontend/src/app/mockData.ts
@@ -3,74 +3,74 @@ import {ProductInfo} from './models/productInfo';
 export const products: ProductInfo[] = [
   {
     productId: 'B0001',
-    productName: 'Core Java',
+    productName: 'Flame CG 100 N 4/4',
     productPrice: 30.00,
     productStock: 96,
-    productDescription: 'Books for learning Java',
-    productIcon: 'https://images-na.ssl-images-amazon.com/images/I/41f6Rd6ZEPL._SX363_BO1,204,203,200_.jpg',
+    productDescription: 'Chitara Clasica Flame CG 100 N 4/4, fata, spate si laterale din placaj laminat.',
+    productIcon: 'https://static.flymusic.ro/img/p/5/9/1/6/4/59164-superlarge_default.jpg',
     productStatus: 0,
     categoryType: 0,
-    createTime: '2018-03-10T11:44:25.000+0000',
-    updateTime: '2018-03-10T11:44:25.000+0000'
+    createTime: '2023-03-10 06:44:25',
+    updateTime: '2023-03-10 06:44:25'
   },
   {
     productId: 'B0002',
-    productName: 'Spring In Action',
+    productName: 'Epiphone Les Paul Studio LT HCS',
     productPrice: 20.00,
     productStock: 195,
-    productDescription: 'Learn Spring',
-    productIcon: 'https://images-na.ssl-images-amazon.com/images/I/51gHy16h5TL._SX397_BO1,204,203,200_.jpg',
+    productDescription: 'Chitara electrica Epiphone Les Paul Studio LT HCS',
+    productIcon: 'https://static.flymusic.ro/img/p/2/2/1/9/4/7/221947-superlarge_default.jpg',
     productStatus: 0,
     categoryType: 0,
-    createTime: '2018-03-10T15:35:43.000+0000',
-    updateTime: '2018-03-10T15:35:43.000+0000'
+    createTime: '2023-03-10 10:35:43',
+    updateTime: '2023-03-10 10:35:43'
   },
   {
     productId: 'B0001',
-    productName: 'Core Java',
+    productName: 'Flame CG 100 N 4/4',
     productPrice: 30.00,
     productStock: 96,
-    productDescription: 'Books for learning Java',
-    productIcon: 'https://images-na.ssl-images-amazon.com/images/I/41f6Rd6ZEPL._SX363_BO1,204,203,200_.jpg',
-    productStatus: 1,
+    productDescription: 'Chitara Clasica Flame CG 100 N 4/4, fata, spate si laterale din placaj laminat.',
+    productIcon: 'https://static.flymusic.ro/img/p/5/9/1/6/4/59164-superlarge_default.jpg',
+    productStatus: 0,
     categoryType: 0,
-    createTime: '2018-03-10T11:44:25.000+0000',
-    updateTime: '2018-03-10T11:44:25.000+0000'
+    createTime: '2023-03-10 06:44:25',
+    updateTime: '2023-03-10 06:44:25'
   },
   {
     productId: 'B0002',
-    productName: 'Spring In Action',
+    productName: 'Epiphone Les Paul Studio LT HCS',
     productPrice: 20.00,
     productStock: 195,
-    productDescription: 'Learn Spring',
-    productIcon: 'https://images-na.ssl-images-amazon.com/images/I/51gHy16h5TL._SX397_BO1,204,203,200_.jpg',
+    productDescription: 'Chitara electrica Epiphone Les Paul Studio LT HCS',
+    productIcon: 'https://static.flymusic.ro/img/p/2/2/1/9/4/7/221947-superlarge_default.jpg',
     productStatus: 0,
     categoryType: 0,
-    createTime: '2018-03-10T15:35:43.000+0000',
-    updateTime: '2018-03-10T15:35:43.000+0000'
+    createTime: '2023-03-10 10:35:43',
+    updateTime: '2023-03-10 10:35:43'
   }, {
     productId: 'B0001',
-    productName: 'Core Java',
+    productName: 'Flame CG 100 N 4/4',
     productPrice: 30.00,
     productStock: 96,
-    productDescription: 'Books for learning Java',
-    productIcon: 'https://images-na.ssl-images-amazon.com/images/I/41f6Rd6ZEPL._SX363_BO1,204,203,200_.jpg',
-    productStatus: 1,
+    productDescription: 'Chitara Clasica Flame CG 100 N 4/4, fata, spate si laterale din placaj laminat.',
+    productIcon: 'https://static.flymusic.ro/img/p/5/9/1/6/4/59164-superlarge_default.jpg',
+    productStatus: 0,
     categoryType: 0,
-    createTime: '2018-03-10T11:44:25.000+0000',
-    updateTime: '2018-03-10T11:44:25.000+0000'
+    createTime: '2023-03-10 06:44:25',
+    updateTime: '2023-03-10 06:44:25'
   },
   {
     productId: 'B0002',
-    productName: 'Spring In Action',
+    productName: 'Epiphone Les Paul Studio LT HCS',
     productPrice: 20.00,
     productStock: 195,
-    productDescription: 'Learn Spring',
-    productIcon: 'https://images-na.ssl-images-amazon.com/images/I/51gHy16h5TL._SX397_BO1,204,203,200_.jpg',
+    productDescription: 'Chitara electrica Epiphone Les Paul Studio LT HCS',
+    productIcon: 'https://static.flymusic.ro/img/p/2/2/1/9/4/7/221947-superlarge_default.jpg',
     productStatus: 0,
     categoryType: 0,
-    createTime: '2018-03-10T15:35:43.000+0000',
-    updateTime: '2018-03-10T15:35:43.000+0000'
+    createTime: '2023-03-10 10:35:43',
+    updateTime: '2023-03-10 10:35:43'
   }];
 
 export const prod: ProductInfo = {
